b70eda7b7128742a189e9bf3a3fefb533334c8c2
Matt Behnke <iterevo@gmail.com>
2017-01-13 11:38:34 -0600

    Added the robot constrants to all the subsystems

--- SteamWorks/src/org/usfirst/frc/team967/robot/subsystems/ClimberSubsystem.java
+++ SteamWorks/src/org/usfirst/frc/team967/robot/subsystems/ClimberSubsystem.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team967.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import org.usfirst.frc.team967.robot.RobotConstraints;
+
 /**
  *
  */
--- SteamWorks/src/org/usfirst/frc/team967/robot/subsystems/DriveSubsystem.java
+++ SteamWorks/src/org/usfirst/frc/team967/robot/subsystems/DriveSubsystem.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team967.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-
+import org.usfirst.frc.team967.robot.RobotConstraints;
 /**
  *
  */
