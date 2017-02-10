11aba01881b884017adccd1ddb2500597bc99085
Matt Behnke <iterevo@gmail.com>
2017-02-09 20:32:33 -0600

    added the start of the vision code
    changed the robot.java robot init file so that it included the creation
    of the camera and started the vision code on the roborio

--- SteamWorks/src/org/usfirst/frc/team967/robot/Robot.java
+++ SteamWorks/src/org/usfirst/frc/team967/robot/Robot.java
@@ -8,6 +8,14 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+
 import org.usfirst.frc.team967.robot.subsystems.DriveSubsystem;
 import org.usfirst.frc.team967.robot.commands.auto.*;
 import org.usfirst.frc.team967.robot.subsystems.CameraSubsystem;
@@ -48,7 +56,22 @@ public class Robot extends IterativeRobot {
 		robotMap = new RobotMap();
     	robotConstraints = new RobotConstraints();
 		oi = new OI();
-		CameraServer.getInstance().startAutomaticCapture();
+		new Thread(() -> {
+            UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+            camera.setResolution(640, 480);
+            
+            CvSink cvSink = CameraServer.getInstance().getVideo();
+            CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+            
+            Mat source = new Mat();
+            Mat output = new Mat();
+            
+            while(!Thread.interrupted()) {
+                cvSink.grabFrame(source);
+                Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+                outputStream.putFrame(output);
+            }
+        }).start();
 		chooser.addDefault("Drive Forward", new driveBaseline());
 		chooser.addObject("LeftBlue", new blueLeftGear());
 		chooser.addObject("RightBlue", new blueRightGear());
